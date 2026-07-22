bd64e1f91947dce4f2285109b158fbc7cbc918b9
Toasters-Main <Toasters-Main@5irobotics.org>
2026-07-21 19:58:02 -0500

    we added localization and a meaningful message to @DAKSHA

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -3,8 +3,8 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
-import com.pedropathing.ftc.drivetrains.Mecanum;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.Encoder;
 import com.pedropathing.ftc.localization.constants.ThreeWheelIMUConstants;
 import com.pedropathing.ftc.localization.localizers.ThreeWheelIMULocalizer;
 import com.pedropathing.paths.PathConstraints;
@@ -14,33 +14,46 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(6)
+            .mass(6);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
-        ThreeWheelIMUConstants constants = new ThreeWheelIMUConstants()
-                .leftEncoder_HardwareMapName("FLeft")
-                .rightEncoder_HardwareMapName("BRight")
-                .strafeEncoder_HardwareMapName("FRight")
-                .IMU_Orientation(new RevHubOrientationOnRobot(
-                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                        RevHubOrientationOnRobot.UsbFacingDirection.RIGHT));
 
         MecanumConstants mecanumConstants = new MecanumConstants()
                 .leftFrontMotorName("FLeft")
                 .rightFrontMotorName("FRight")
                 .leftRearMotorName("BLeft")
                 .rightRearMotorName("BRight")
-                .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+                .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
                 .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
                 .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-                .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+                .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+
+        ThreeWheelIMUConstants localizerConstants = new ThreeWheelIMUConstants()
+//                      .forwardTicksToInches(.001989436789)
+//                      .strafeTicksToInches(.001989436789)
+//                      .turnTicksToInches(.001989436789)
+                .leftPodY(8.125)
+                .rightPodY(-8.125)
+                .strafePodX(-6.75)
+                // ?? add IMU name
+                .leftEncoder_HardwareMapName("FRight")
+                .rightEncoder_HardwareMapName("BRight")
+                .strafeEncoder_HardwareMapName("FLeft")
+                .IMU_Orientation(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.RIGHT));
+                // ?? hey maybe change odometry encoder direction
+//                         .leftEncoderDirection(Encoder.FORWARD)
+//                         .rightEncoderDirection(Encoder.FORWARD)
+//                         .strafeEncoderDirection(Encoder.FORWARD);
 
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .mecanumDrivetrain(mecanumConstants)
                 .pathConstraints(pathConstraints)
-                .setLocalizer(new ThreeWheelIMULocalizer(hardwareMap, constants))
+                .threeWheelIMULocalizer(localizerConstants)
+
                 .build();
+//                      .setLocalizer(new ThreeWheelIMULocalizer(hardwareMap, constants)).build();
+
     }
 }
